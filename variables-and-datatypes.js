3711243ab2329ca5006fb9ed02535bda60054245
Vasanti-Vaghela <vasantivaghela2812@gmail.com>
2024-12-24 19:29:23 +0530

    variables and datatypes

---
+++ variables-and-datatypes.js
@@ -0,0 +1,27 @@
+// Create a const object called "product" to store information about ball pen.
+const product = {
+  title: "Ball Pen",
+  rating: 4,
+  offer: 5,
+  price: 270,
+};
+
+console.log(product);
+console.log(typeof product);
+console.log("5" - 5); // string 5 converted to number
+console.log("5" + 5); // here concatenation is done
+console.log(5 + false); // false treated as 0
+console.log(5 + true); // true treated  as 1
+
+// Create a const object called "profile" to store information about a person.
+
+const profile = {
+  username: "Vasanti Vaghela",
+  followers: 567,
+  following: 5,
+  isFollow: true,
+  occupation: "Frontend Developer",
+};
+
+console.log(profile);
+console.log(typeof profile.isFollow);
